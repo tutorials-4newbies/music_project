793f527416c37a801128c3a74a3fc334b25c52c4
Alon Palgy <a.palgy@gmail.com>
2021-12-20 15:46:30 +0200

    added more fields to song and a migration

--- songs_list/models.py
+++ songs_list/models.py
@@ -5,3 +5,5 @@ class Song(models.Model):
     name = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
     album = models.CharField(max_length=200)
+    release_year = models.DateField(null=True)
+    youtube_link = models.URLField(null=True)
