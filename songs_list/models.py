e8a73122d8d91fea3b333592dcb6215199060a49
Alon Palgy <a.palgy@gmail.com>
2021-12-20 14:44:00 +0200

    added a song model

--- songs_list/models.py
+++ songs_list/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    name = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    album = models.CharField(max_length=200)
