e91dc897f09767f72458a345f2a4b93c9b01705b
Alon Palgy <a.palgy@gmail.com>
2022-01-24 18:56:07 +0200

    added more fields and a method to return year if it is not None

--- songs_list/views.py
+++ songs_list/views.py
@@ -18,9 +18,16 @@ def get_all_songs(request):
     songs = Song.objects.all()
     content = []
     for song in songs:
+        if song.release_year is not None:
+            year = song.release_year.strftime("%Y")
+        else:
+            year = None
         content.append({
             "id": song.id,
             "name": song.name,
+            "album": song.album,
+            "release_year": year,
+            "youtube_link": song.youtube_link
         })
 
     return JsonResponse({"songs": content})
