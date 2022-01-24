83304507163349d2282ba3415df3bb8e47a7cc90
Alon Palgy <a.palgy@gmail.com>
2022-01-24 18:44:56 +0200

    added a view for song list as json

--- songs_list/urls.py
+++ songs_list/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('songs/', views.get_all_songs, name='get_all_songs')
 ]
--- songs_list/views.py
+++ songs_list/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 from songs_list.models import Song, Artist
 
@@ -11,3 +12,15 @@ def index(request):
         "artists": artists
     }
     return render(request, 'songslist/index.html', context)
+
+
+def get_all_songs(request):
+    songs = Song.objects.all()
+    content = []
+    for song in songs:
+        content.append({
+            "id": song.id,
+            "name": song.name,
+        })
+
+    return JsonResponse({"songs": content})
