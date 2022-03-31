32d750f59d224cf61a079183c50873ea50829923
Alon Palgy <a.palgy@gmail.com>
2022-03-31 08:50:19 +0300

    fixed missing underscore in songs_list name

--- songs_list/views.py
+++ songs_list/views.py
@@ -6,9 +6,7 @@ from songs_list.models import Song, Artist
 
 def index(request):
     songs = Song.objects.all()
-    artists = Artist.objects.all()
     context = {
         "songs": songs,
-        "artists": artists
     }
-    return render(request, 'songslist/index.html', context)
+    return render(request, 'songs_list/index.html', context)
